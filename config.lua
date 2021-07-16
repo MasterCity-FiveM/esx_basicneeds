15b364dfe074cfdab5b8b1d3f17bad5fa14a2dad
masterking32 <lichwow_masterking@yahoo.com>
2021-07-16 05:51:01 +0430

    new foods.

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ Config = {}
 Config.Locale = 'fa'
 
 Config.Drinks = {'water', 'monster', 'hype', 'kapochino', 'nooshmak', 'shirkakaoo', 'zizigooloo', 'fanta', 'redbull', 'sandis'}
-Config.Food = {'bread', '5dollartaco', 'chickensandwich', 'hamburger', 'heartstopper', 'macka', 'moneyshot', 'pizza', 'cheesebows', 'chips', 'pampam'}
+Config.Food = {'bread', '5dollartaco', 'chickensandwich', 'hamburger', 'heartstopper', 'macka', 'moneyshot', 'pizza', 'cheesebows', 'chips', 'pampam', 'NormalSushi', 'SpecialSushi', 'PandaSushi', 'Meygoo', 'SpecialMeygoo'}
